848260576af3feee9915db43c2a4b01b29b753cc
Pradeep M <125256248+Pradeep-848@users.noreply.github.com>
2025-07-27 11:53:40 +0530

    Add files via upload

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,27 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import SeatSelection from "../pages/SeatSelection";
+import Footer from "../components/Footer";
+import TicketPage from "../pages/TicketPage";
+import Notification from "../components/Notification";
+
+function App() {
+  return (
+    <Router>
+      <Notification />
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/select-seats" element={<SeatSelection />} />
+            <Route path="/get-tickets" element={<TicketPage />} />
+          </Routes>
+        </div>
+        {/* <Footer /> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
